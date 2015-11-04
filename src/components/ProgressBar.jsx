c70acf7fdc38f8a6d26f4be15b8d86c363e438e1
tjallingt <tjallingt@gmail.com>
2015-11-04 16:04:14 +0100

    Added basic progressbar Since the progressbar is hidden by the `playerVars` another progressbar is needed. This has the basic functionallity required (showing the progress). Might be extended in the future...

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+ 
+export default class ProgressBar extends React.Component {
+	static propTypes = {
+		now: React.PropTypes.number.isRequired,
+	};
+
+	static defaultProps = {
+		now: 0
+	};
+
+	constructor( props ) {
+		super( props );
+	}
+
+	render() {
+		const styles = {
+			wrapper: {
+				
+			},
+			progress: {
+				backgroundColor: "#F00",
+				width: `${this.props.now}%`,
+				height: 5,
+				transition: "width 250ms"
+
+			}
+		};
+
+		Object.assign( styles.wrapper, this.props.style );
+
+		return (
+			<div id={this.props.id} style={styles.wrapper}>
+				<div style={styles.progress}></div>
+			</div>
+		);
+	}
+}
